db7b45085e19f245222f03ccaa90be3eb60ec780
Cheng Li <lobatt@Chengs-MacBook-Pro.local>
2018-04-05 04:17:01 -0400

    License tweak

--- ATC20.js
+++ ATC20.js
@@ -1,19 +1,19 @@
-// Copyright (C) 2017 go-nebulas authors
+// Copyright (C) 2017 Atlas Protocol authors
 //
-// This file is part of the go-nebulas library.
+// This file is part of the prototype implementation of Atlas Protocol
 //
-// the go-nebulas library is free software: you can redistribute it and/or modify
+// The Atlas Protocol is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// the go-nebulas library is distributed in the hope that it will be useful,
+// This prototype implementation is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with the go-nebulas library.  If not, see <http://www.gnu.org/licenses/>.
+// along with the code.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 'use strict';
