cdd317856def7778f9bc720f682c56dffc19c0ec
Atlas Protocol <lobatt@Chengs-MacBook-Pro.local>
2018-04-05 04:39:09 -0400

    License wording

--- ATC20.js
+++ ATC20.js
@@ -13,7 +13,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with the code.  If not, see <http://www.gnu.org/licenses/>.
+// along with the code. If not, see <http://www.gnu.org/licenses/>.
 //
 
 'use strict';
